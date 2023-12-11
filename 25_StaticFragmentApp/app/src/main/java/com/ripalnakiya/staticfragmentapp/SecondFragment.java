ef2a3934d6f0a87359335062c6081c7bb7440250
Ripal Nakiya <ripalvnakiya5645@gmail.com>
2023-12-11 16:26:53 +0530

    Added 25 Static Fragment App

---
+++ 25_StaticFragmentApp/app/src/main/java/com/ripalnakiya/staticfragmentapp/SecondFragment.java
@@ -0,0 +1,26 @@
+package com.ripalnakiya.staticfragmentapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class SecondFragment extends Fragment {
+    public SecondFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+
+        // Getting access to the values of XML FIle
+        TextView textview2 = view.findViewById(R.id.textview2);
+
+        return view;
+    }
+}
